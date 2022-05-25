916a59026d84dbf6c297bfd4fcc87a2ab55703fa
Rodrigo Rapoport <rodrigorapoport@MacBook-Air-de-Rodrigo.local>
2022-05-25 15:13:48 -0300

    Cambios en la calculadora de inversion v2

--- js/calcul.js
+++ js/calcul.js
@@ -8,6 +8,10 @@
         var carbon_credits_anual = carbon_anual*11;
         var returns_harvest = usd*0.2;
         var land_appreciation = usd*0.09;
+        var in_5 = usd*0.5;
+        var in_2 = usd*0.2;
+        var in_1 = usd*0.1;
+        
     //Obtienes el ids
         
         var total_amount_arvol= document.getElementById('total_amount_arvol');
@@ -16,13 +20,20 @@
    
         var returns_harvest_annual = document.getElementById('returns_harvest_annual');
         var land_appreciation_annual = document.getElementById('land_appreciation_annual');
+        var inv_50 = document.getElementById('inv_50');
+        var inv_w = document.getElementById('inv_w');
+        var inv_c = document.getElementById('inv_c');
+        var inv_o = document.getElementById('inv_o');
 
 
          //agrega los resultados al DOM
-        total_amount_arvol.innerHTML = Number(total_av.toFixed(2));
-        carbon_capture.innerHTML =  Number(carbon_anual.toFixed(2));
-        carbon_credits_annual.innerHTML = Number(carbon_credits_anual.toFixed(2));
-        returns_harvest_annual.innerHTML = Number(returns_harvest.toFixed(2));
-        land_appreciation_annual.innerHTML = Number(land_appreciation.toFixed(2)); 
-    
+        total_amount_arvol.innerHTML = total_av.toFixed(2);
+        carbon_capture.innerHTML =  carbon_anual.toFixed(2);
+        carbon_credits_annual.innerHTML = carbon_credits_anual.toFixed(2);
+        returns_harvest_annual.innerHTML = returns_harvest.toFixed(2);
+        land_appreciation_annual.innerHTML = land_appreciation.toFixed(2); 
+        inv_50.innerHTML = in_5.toFixed(2);
+        inv_w.innerHTML = in_2.toFixed(2);
+        inv_c.innerHTML = in_2.toFixed(2);
+        inv_o.innerHTML = in_5.toFixed(2);
     }
